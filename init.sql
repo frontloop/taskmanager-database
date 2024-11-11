eb9841fd21784d3ee1491a2afee376ed7377ff60
Fedor Kotov <kotov@gmx.net>
2024-11-11 16:34:07 +0100

    init

---
+++ init.sql
@@ -0,0 +1,34 @@
+CREATE TABLE public.task
+(
+    id numeric,
+    name character varying(200) COLLATE pg_catalog."default",
+    done boolean,
+    created timestamp(6) with time zone NOT NULL,
+    priority character varying(30) COLLATE pg_catalog."default"
+);
+
+ALTER TABLE public.task OWNER TO postgres;
+
+CREATE SEQUENCE public.task_id_generator
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.task_id_generator OWNER TO postgres;
+
+ALTER TABLE ONLY public.task ALTER COLUMN id SET DEFAULT nextval('public.task_id_generator'::regclass);
+
+COPY public.task (id, name, done, created, priority) FROM stdin;
+1	Task 1	false	2024-11-11 11:29:21.99998+00	NORMAL
+2	Task 2	false	2024-11-11 11:30:21.99998+00	LOW
+\.
+
+SELECT pg_catalog.setval('public.task_id_generator', 2, true);
+
+
+ALTER TABLE ONLY public.task
+    ADD CONSTRAINT task_pkey PRIMARY KEY (id);
